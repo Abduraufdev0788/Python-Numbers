b0cfa8d16c6e33f07d4353e7986cb577e7adf27e
abduraufdev0788 <abduraufnasrullayev790@gmail.com>
2025-05-24 17:16:06 +0500

    taxi puli bajarildi

--- taxi_fare_calculator.py
+++ taxi_fare_calculator.py
@@ -1,1 +1,9 @@
+from num2words import num2words
+masofa = float(input("masofanini kiriting kmda: "))
+km_uchun =masofa * 1.20
+jami = 3.00 + km_uchun
+eng = num2words(round(jami,2))
+ru = num2words(round(jami,2), lang='ru')
+print(f"jami: {round(jami,2)}$, {eng} dollar ; {ru} dоллар")
+
 
