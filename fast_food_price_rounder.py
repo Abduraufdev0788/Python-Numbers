3bc714084b2cba9497b4bdc60e229947569eabf9
abduraufdev0788 <abduraufnasrullayev790@gmail.com>
2025-05-24 16:33:02 +0500

    bajarildi sum va split funksiyalari orqali

--- fast_food_price_rounder.py
+++ fast_food_price_rounder.py
@@ -1,1 +1,9 @@
-
+from  num2words import num2words
+narx = (input("narxlarni vergul bilan ajratib kiriting: "))
+a = narx.split(",")
+b = [float(i) for i in a]
+c = sum(b)
+d = num2words(c)
+e = num2words(c, lang = "ru")
+print("Jami narx: ", round(c, 1))
+print("jami summa: ", d,"; ",e)
