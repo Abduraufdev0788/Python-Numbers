540a3818ff8b915f9768deae3411888f725ad88c
abduraufdev0788 <abduraufnasrullayev790@gmail.com>
2025-05-24 15:58:35 +0500

    tuzatildi

--- cash_denomination_splitter.py
+++ cash_denomination_splitter.py
@@ -1,3 +1,4 @@
+from num2words import num2words
 amount = int(input("Pul miqdorini kiriting ($): "))
 
 k_50 = amount // 50
@@ -15,4 +16,10 @@ print("$5 kupyuradan:", k_5, "ta")
 k_1 = amount // 1
 amount = amount % 1
 print("$1 kupyuradan:", k_1, "ta")
+
+a = num2words(k_50*50 + k_10*10 + k_5*5 + k_1*1)
+b = num2words(k_50*50 + k_10*10 + k_5*5 + k_1*1, lang = "ru")
+
+print( "Umumiy summa: ",a, ",", b)
+
 
