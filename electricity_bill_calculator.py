e23e42393eac29c0889fb1273dd5e49c3f3ec94a
abduraufdev0788 <abduraufnasrullayev790@gmail.com>
2025-05-24 17:28:47 +0500

    bajarildi

--- electricity_bill_calculator.py
+++ electricity_bill_calculator.py
@@ -1,1 +1,7 @@
-
+from num2words import num2words
+start = float(input("oyning boshidagi qiymatni kiriting (kWh): "))
+end = float(input("oyning oxiridagi qiymatni kiriting (kWh): "))
+free = (end - start) * 0.45
+a = num2words(round(free, 2),)
+b = num2words(round(free, 2), lang='ru')
+print(f"Elektr energiyasi uchun to'lov: {free:.2f} $\n {a} dollar\n {b} dоллар")
