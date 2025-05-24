1017a0c3ae65145ddfc15f1585ab8dc5b5612ca8
abduraufdev0788 <abduraufnasrullayev790@gmail.com>
2025-05-24 17:18:39 +0500

    kruyer puli bajarildi

--- delivery_service_price.py
+++ delivery_service_price.py
@@ -1,1 +1,8 @@
+from num2words import num2words
+masofa = float(input("masofanini kiriting kmda: "))
+km_uchun =masofa * 0.80
+jami = 5.00 + km_uchun
+eng = num2words(round(jami,2))
+ru = num2words(round(jami,2), lang='ru')
+print(f"jami: {round(jami,2)}$, {eng} dollar ; {ru} dоллар")
 
